6c25e25d97b5d89fbee8be1dc07eecc25ae0e25b
Trisha Basak <trishabasak2005@gmail.com>
2025-11-10 22:24:29 +0530

    Delete day1-micro directory

--- day1-micro/depts-microservice 2/src/main/java/test/depts/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package test.depts.repository;
-
-import test.depts.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long>
-{
-    Department findByDepartmentCode(String departmentCode);
-}
